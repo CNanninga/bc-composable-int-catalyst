37887540b15497ca2eb289e9af63eb4340951a63
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-04-04 17:17:38 -0500

    Insert product FAQs: Create client component

--- components/product-faqs/index.tsx
+++ components/product-faqs/index.tsx
@@ -16,9 +16,22 @@ const ProductFaqs = ({
   loadMoreLabel: string;
   faqData: Awaited<ReturnType<typeof getProductFaqMetafields>>;
 }) => {
+  const [faqs, setFaqs] = useState(faqData.faqs);
+
   return (
     <>
-      
+      {faqs.map((faq) => (
+        <div className="my-4" key={faq.key}>
+          <div>
+            <label className="font-bold">Question:</label>
+            <span> {faq.question}</span>
+          </div>
+          <div>
+            <label className="font-bold">Answer:</label>
+            <span> {faq.answer}</span>
+          </div>
+        </div>
+      ))}
     </>
   );
 };
